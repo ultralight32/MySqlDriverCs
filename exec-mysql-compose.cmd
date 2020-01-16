e3cd20a0b64e03a7d8526d3537eeea10a5824d42
mlucas <mlucas@voxelgroup.net>
2020-01-16 23:16:00 +0100

    try to pass tests on

---
+++ exec-mysql-compose.cmd
@@ -0,0 +1,1 @@
+docker-compose -f docker-compose-mysql.yml up --build --abort-on-container-exit
--- exec-mysql.cmd
+++ exec-mysql.cmd
@@ -1,14 +1,27 @@
-rem docker-compose -f docker-compose-mysql.yml up --build --abort-on-container-exit
-rem docker stop some-mysql
-rem docker rm some-mysql
-rem docker run --name some-mysql -e MYSQL_ROOT_PASSWORD=pass -e MYSQL_DATABASE=db -e MYSQL_USER=user -e MYSQL_PASSWORD=pass -d mysql:latest
-rem docker exec -it some-mysql bash
-rem  mysql -hsome-mysql -uuser -p
+rem remove images
+docker system prune -a
 
-docker stop mysql-8
-docker rm mysql-8
+rem create a net
+rem docker network create mynet
+
+rem stop mysql_container
+docker stop mysql_container
+docker rm mysql_container
+
+rem pull and start mysql_container
 docker pull mysql/mysql-server
-docker run -p 3307:3306 --name mysql-8 -e MYSQL_ROOT_PASSWORD=pass -e MYSQL_DATABASE=mydb --restart always -d mysql/mysql-server:latest
-docker logs mysql-8
-docker exec -it mysql-8 bash
-rem  mysql -uroot -ppass mydb
+docker run -p 3307:3306 --name mysql_container -e MYSQL_ROOT_PASSWORD=pass -e MYSQL_DATABASE=mydb --restart always -d mysql/mysql-server:latest
+
+rem stop mysqldriver_container
+docker stop mysqldriver_container
+docker rm mysqldriver_container
+
+rem build image from sources
+docker build -t mysqldriver_image -f dockerfile-build .
+
+
+rem run test image
+docker run --link mysql_container:mysql_container --name mysqldriver_container -e "CONNECTION_STRING=Location=mysql_container;Data Source=todosdb;User ID=user;Password=pass;Port=3307" mysqldriver_image dotnet test ./src/MySqlDriverCs.Core.Tests/MySqlDriverCs.Core.Tests.csproj
+
+rem docker run -it --link mysql_container:mysql_container  --entrypoint=bash --name mysqldriver_container -e "CONNECTION_STRING=Location=mysql_container;Data Source=todosdb;User ID=user;Password=pass;Port=3307" mysqldriver_image
+pause
